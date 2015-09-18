59b176a43d81afef1f79069b042d149e5fdea95c
Michael Kutý <6du1ro.n@gmail.com>
2015-09-18 08:55:00 +0200

    fix admin doc url

--- leonardo_admin/urls.py
+++ leonardo_admin/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import include, patterns, url
 from leonardo.site import leonardo_admin
 
 urlpatterns = patterns('',
-                       url(r'^doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        url(r'^admin/', include(leonardo_admin.urls)),
                        )
